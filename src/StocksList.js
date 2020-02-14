15cc6ba5ed6c9c093e81e4f0b948f28590113848
Sergio <solivares1178@gmail.com>
2020-02-14 17:55:32 -0500

    fetching added

--- src/StocksList.js
+++ src/StocksList.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
+import stocksList from "./list_company.json";
 
 // this is the API https://financialmodelingprep.com/api/v3/stock/real-time-price/${stock}
 //that I want to loop to have "Symbol" - "Name" - "price"
@@ -8,6 +9,32 @@ import axios from "axios";
 const StocksList = props => {
   const stocksData = props.stocksList.stocksList;
 
+  //Fetching
+  //TODO: Doesn't get the stocksData.price value
+  const fetchData = stocksData.map((stock, index) => {
+    if (index < 10) {
+      let stockData;
+      fetch(
+        `https://financialmodelingprep.com/api/v3/stock/real-time-price/${stock.Symbol}`
+      ).then(response => {
+        if (!response.ok) {
+          console.log("no status : 200");
+          throw new Error(`Status Code Error: ${response.status}`);
+        } else {
+          response.json().then(data => {
+            stockData = data;
+            console.log(stockData.symbol + stockData.price);
+          });
+        }
+      });
+      return (
+        <li>
+          {index + 1} -{stock.Symbol} -{stock.Name} - {stocksData.price}
+        </li>
+      );
+    }
+  });
+  // Stock List
   const stocks = stocksData.map((stock, index) => {
     if (index < 10) {
       return (
@@ -22,6 +49,8 @@ const StocksList = props => {
     <div>
       <h1> Stock List</h1>
       <ul>{stocks}</ul>
+      <h2>Fetching</h2>
+      <ul>{fetchData}</ul>
     </div>
   );
 };
